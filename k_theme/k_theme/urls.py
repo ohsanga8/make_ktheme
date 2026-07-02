0faf71bc53bb06949628ead1b0c41c5e80204be4
오상아 <115799357+ohsanga8@users.noreply.github.com>
2026-07-02 15:48:18 +0900

    urls.py 업데이트

--- k_theme/k_theme/urls.py
+++ k_theme/k_theme/urls.py
@@ -26,8 +26,6 @@ urlpatterns = [
     path("", include("main.urls")),
     path("auth/", include("authapp.urls")),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
