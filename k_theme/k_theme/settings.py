7b69f937db14b6edb4b0dd2fa2c3615a75421b52
ohsanga8@naver.com <ohsanga8@naver.com>
2026-07-02 16:24:15 +0900

    Cloudinary

--- k_theme/k_theme/settings.py
+++ k_theme/k_theme/settings.py
@@ -47,8 +47,19 @@ INSTALLED_APPS = [
     "main",
     "authapp",
     "django_extensions",
+
+    'cloudinary_storage',
+    'cloudinary',
     # "main.apps.MainConfig",
 ]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
